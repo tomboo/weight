c738fda20f413d60d343e7c1c0f5e8c253ae52bf
Tom Booster <Tom@Toms-iMac.local>
2016-02-05 18:05:41 -0800

    Add plots.

--- playground.py
+++ playground.py
@@ -7,11 +7,38 @@ This is a temporary script file.
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+# import datetime
 
+DATAFILE = 'data.csv'
+WEIGHT = 'Weight'
+AVGWEIGHT = 'Avg. Weight'
+
+def plot(df):
+    dt = df.index.to_pydatetime()
+    x = mdates.date2num(dt)
+    fit = np.polyfit(x, df[AVGWEIGHT], 1)
+    fit_fn = np.poly1d(fit)
+    # fit_fn is now a function which takes in x and returns as estimate for y
+
+    xx = [x[0], x[-1]]
+
+    df.plot(grid=True)
+    ax = plt.gca()
+    ax.plot(xx, fit_fn(xx))       
+    
 def main():
-    x = pd.read_csv('data.csv')
-    print(x.head())
-    x.plot()
+    df = pd.read_csv(DATAFILE, index_col=0, parse_dates=True)
+    # Exponentially weighted moving average (ewma)
+    df[AVGWEIGHT] = pd.ewma(df[WEIGHT], span=20)
+    print(df.tail())
+    print(df.describe())
+    plot(df)            # max
+    plot(df['2015'])    # last year
+    plot(df['2016'])    # this year
+    plot(df['2016-01']) # last month
+    plot(df['2016-02']) # this month
 
 if __name__ == '__main__':
     main()
