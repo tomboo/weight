c7749709190281c7639d88a80c12c6609ddf0710
Tom Booster <tom.booster@gmail.com>
2016-03-29 07:19:09 -0700

    Ongoing

--- weight/model.py
+++ weight/model.py
@@ -9,8 +9,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-from matplotlib import style
-style.use('ggplot')
+plt.style.use('ggplot')
 
 
 # TODO: need a better way to reference datafiles
